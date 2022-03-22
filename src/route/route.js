776b0be687bcff8d5aaf63150f4515bf7ad8839a
md <ansarirashid771@gmail.com>
2022-03-22 11:30:16 +0530

    recent development

--- src/route/route.js
+++ src/route/route.js
@@ -1,8 +1,10 @@
 const express = require('express'); 
 const router = express.Router(); 
 const collegeController = require('../controller/college.controller'); 
+const internController=require('../controller/intern.controller');
 
 router.post('/colleges', collegeController.createCollege); 
+router.post('/interns',internController.createIntern);
 
 
 
