d8572819608ce54fcd8777f00e3a8f0740a12fab
Prakash Sah <prakashsah10091@gmail.com>
2022-03-24 17:27:38 +0530

    short client error code

--- src/helpers/clientError.js
+++ src/helpers/clientError.js
@@ -3,32 +3,20 @@ const mongoose = require('mongoose');
 const handleError = (res, error)=>{
     if(error['errors'] != null){  
         const key = Object.keys(error['errors']);
-        const requiredField = [], uniqueField = [];
-        key.forEach((key) => {
-            if (error['errors'][key]['kind'] === "required") {
-                requiredField.push(error['errors'][key]['message']);
-            }
-            else if(error['errors'][key]['kind'] === "user defined"){
-                requiredField.push(error['errors'][key]['message']);
-            }
-            else if(error['errors'][key]['kind'] === "regexp"){
-                requiredField.push(error['errors'][key]['message']);
-            }
-            else if (error['errors'][key]['kind'] === "unique") {
-                uniqueField.push(error['errors'][key]['message']);
-            }
-        });
-        if (requiredField.length > 0) {
+        const errorField = []; 
+        key.forEach((key)=>{
+            let errorType = error['errors'][key]; 
+            errorField.push({
+                type: errorType['kind'],
+                message: errorType['message']
+            }); 
+        }); 
+        if (errorField.length > 0) {
             return res.status(400).send({
                 status: false,
-                message: requiredField
+                message: errorField
             });
         }
-        if(uniqueField.length > 0)
-        return res.status(400).send({  //409
-            status: false,
-            message: uniqueField
-        }); 
     }
     return res.status(500).send({
         status: false,
